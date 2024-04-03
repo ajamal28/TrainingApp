2498fc5c90f4fb908e404b3c409e4894ebe70796
Abdullahi Jamal <abdullahijamal08@gmail.com>
2024-04-03 13:39:47 +0100

    version gallery

--- assets/app.js
+++ assets/app.js
@@ -7,4 +7,61 @@ import './bootstrap.js';
  */
 import './styles/app.css';
 
+document.getElementById("openModal1").addEventListener("click", function() {
+  openModal(1);
+});
+document.getElementById("openModal2").addEventListener("click", function() {
+  openModal(2);
+});
+document.getElementById("openModal3").addEventListener("click", function() {
+  openModal(3);
+});
+document.getElementById("closeModal").addEventListener("click", closeModal);
+document.getElementById("prev1").addEventListener("click", plusSlides);
+document.getElementById("next1").addEventListener("click", minusSlides);
 
+function openModal(initialSlideIndex) {
+  slideIndex = initialSlideIndex;
+  document.getElementById("myModal").style.display = "block";
+  showSlides(slideIndex);
+}
+
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+
+function plusSlides() {
+  slideIndex += 1;
+  showSlides(slideIndex);
+}
+
+function minusSlides() {
+  slideIndex -= 1;
+  showSlides(slideIndex);
+}
+
+showSlides(slideIndex);
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+
+  if (n > slides.length) {
+      slideIndex = 1;
+  }
+  if (n < 1) {
+      slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
+
+  
+
+
+  
+ 
